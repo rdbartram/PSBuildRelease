f7adc665977c403546f477b786b27e02ea16cd58
Ryan Bartram <ryan@bartram.ch>
2018-12-18 21:00:14 +0100

    Remove conditions for gitversion tasks

--- BuildTasks/gitversion.build.ps1
+++ BuildTasks/gitversion.build.ps1
@@ -1,10 +1,10 @@
-task gitversion -before CreateModuleManifest, IncrementScriptVersion -If ((get-command gitversion -ErrorAction SilentlyContinue) -and -not $env:SYSTEM_ACCESSTOKEN) {
+task gitversion {
     $GitVersionInfo = GitVersion | ConvertFrom-Json
     $env:GITVERSION_SEMVER = $GitVersionInfo.SemVer
     $env:GITVERSION_ASSEMBLYSEMVER = $GitVersionInfo.AssemblySemVer
 }
 
-task fakegitversion -before CreateModuleManifest, IncrementScriptVersion -If (-not (get-command gitversion -ErrorAction SilentlyContinue) -and -not $env:SYSTEM_ACCESSTOKEN) {
+task fakegitversion {
     Write-Warning 'gitversion was not found. The supplied version is a default and not necessarily useful'
     $env:GITVERSION_SEMVER = "1.0.0-local.1"
     $env:GITVERSION_ASSEMBLYSEMVER = "1.0.0.0"
