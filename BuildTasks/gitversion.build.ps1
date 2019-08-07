86b507635d55fe4721ec44c96cfa62684564c3cd
Ryan Bartram <ryan@bartram.ch>
2019-08-07 17:39:26 +0200

    Update gitversion.build.ps1

--- BuildTasks/gitversion.build.ps1
+++ BuildTasks/gitversion.build.ps1
@@ -1,11 +1,11 @@
-task GitVersion -before CreateModuleManifest, IncrementScriptVersion -If ((get-command gitversion -ErrorAction SilentlyContinue) -and -not $env:SYSTEM_ACCESSTOKEN) {
+task GitVersion -before CreateModuleManifest, IncrementScriptVersion -If ((get-command gitversion -ErrorAction SilentlyContinue) -and ($null -ne $env:GITVERSION_NuGetVersionV2)) {
     $gitVersionInfo = GitVersion | ConvertFrom-Json
     $env:GITVERSION_MajorMinorPatch = $gitVersionInfo.MajorMinorPatch
     $env:GITVERSION_NuGetPreReleaseTagV2 = $gitVersionInfo.NuGetPreReleaseTagV2
     $env:GITVERSION_NuGetVersionV2 = $gitVersionInfo.NuGetVersionV2
 }
 
-task FakeGitVersion -before CreateModuleManifest, IncrementScriptVersion -If (-not (get-command gitversion -ErrorAction SilentlyContinue) -and -not $env:SYSTEM_ACCESSTOKEN) {
+task FakeGitVersion -before CreateModuleManifest, IncrementScriptVersion -If (-not (get-command gitversion -ErrorAction SilentlyContinue) -and ($null -ne $env:GITVERSION_NuGetVersionV2)) {
     Write-Warning 'gitversion was not found. The supplied version is a default and not necessarily useful'
     $env:GITVERSION_MajorMinorPatch = "1.0.0"
     $env:GITVERSION_NuGetPreReleaseTagV2 = ""
