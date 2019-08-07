ff102edc5a6d38d5de0c0729d67bb020c1f34250
Ryan Bartram <ryan@bartram.ch>
2019-08-07 23:37:01 +0200

    fix logic for invoke build gitversion

--- BuildTasks/gitversion.build.ps1
+++ BuildTasks/gitversion.build.ps1
@@ -1,11 +1,11 @@
-task GitVersion -before CreateModuleManifest, IncrementScriptVersion -If ((get-command gitversion -ErrorAction SilentlyContinue) -and ($null -ne $env:GITVERSION_NUGETVERSIONV2)) {
+task GitVersion -before CreateModuleManifest, IncrementScriptVersion -If ((get-command gitversion -ErrorAction SilentlyContinue) -and ($null -eq $env:GITVERSION_SEMVER)) {
     $gitVersionInfo = GitVersion | ConvertFrom-Json
     $env:GITVERSION_MAJORMINORPATCH = $gitVersionInfo.MAJORMINORPATCH
     $env:GITVERSION_NUGETPRERELEASETAGV2 = $gitVersionInfo.NUGETPRERELEASETAGV2
     $env:GITVERSION_NUGETVERSIONV2 = $gitVersionInfo.NUGETVERSIONV2
 }
 
-task FakeGitVersion -before CreateModuleManifest, IncrementScriptVersion -If (-not (get-command gitversion -ErrorAction SilentlyContinue) -and ($null -ne $env:GITVERSION_NUGETVERSIONV2)) {
+task FakeGitVersion -before CreateModuleManifest, IncrementScriptVersion -If (-not (get-command gitversion -ErrorAction SilentlyContinue) -and ($null -eq $env:GITVERSION_SEMVER)) {
     Write-Warning 'gitversion was not found. The supplied version is a default and not necessarily useful'
     $env:GITVERSION_MAJORMINORPATCH = "1.0.0"
     $env:GITVERSION_NUGETPRERELEASETAGV2 = ""
